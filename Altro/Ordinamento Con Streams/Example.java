b4a4f8b9d72517bd1ac73bcf5abd7e85baae5abd
Davide Randino <89197609+daviderandino@users.noreply.github.com>
2023-06-24 13:00:55 +0200

    Update Example.java

--- Altro/Ordinamento Con Streams/Example.java
+++ Altro/Ordinamento Con Streams/Example.java
@@ -10,10 +10,20 @@ public class Example {
         liste.put(l.getNome(),l);
     }
 
+
+    // ordinamento decrescente
     public Collection<Lista> getRisultatiListe(){
 
         return liste.values().stream()
                 .sorted((l1,l2) -> (int) (l2.getNumVoti() - l1.getNumVoti()))
+                .collect(Collectors.toList());
+    }
+
+    // ordinamento crescente
+     public Collection<Lista> getRisultatiListe(){
+
+        return liste.values().stream()
+                .sorted((l1,l2) -> (int) (l1.getNumVoti() - l2.getNumVoti()))
                 .collect(Collectors.toList());
     }
 
